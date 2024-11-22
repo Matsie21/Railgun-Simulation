5aac89e6174b310726e926a8013625383713aa3c
BuildTools <unconfigured@null.spigotmc.org>
2024-11-22 11:10:24 +0100

    More variables

--- RailGun.cpp
+++ RailGun.cpp
@@ -3,46 +3,62 @@
 #include <conio.h>
 #include "progressbar.hpp"
 
-int main() {
+//Quickly change standard datatype
+using t_simfloat = double;
 
-    /*
+/*
         Changeable variables
-    */
+*/
 
-    //Friction coefficients between armature, and rails and plates
-    const double mu_s = 0;
-    const double mu_k = 0;
-    
-    //Properties of individual rails
-    const double l_r = 0;
-    const double w_r = 0;
-    const double h_r = 0;
-    const double dens_r = 0;
-    const double resistiv_r = 0;
-    const double SpecHeat_r = 0;
+//General simulation settings
+const t_simfloat t = 0;
+const t_simfloat dt_in = 0;     //Timestep inside railgun
+const t_simfloat dt_out = 0;    //Timestep outside railgun
 
-    //Properties of armature
-    const double l_a = 0;
-    const double w_a = 0;
-    const double h_a = 0;
-    const double dens_a = 0;
-    const double resistiv_a = 0;
-    const double SpecHeat_a = 0;
-    const double alpha_V = 0;
-    
-    //Properties of plates
-    const double h_pl = 0;
-    const double SpecHeat_pl = 0;
+//Standard units
+const t_simfloat AirDens = 0;
+const t_simfloat g = 0;
 
-    //Properties of power wires
-    const double l_pw = 0;
-    const double A_pw = 0;
-    const double resistiv_pw = 0;
+//Friction coefficients between armature, and rails and plates
+const t_simfloat mu_s = 0;
+const t_simfloat mu_k = 0;
 
-    //Properties of powersupply
-    bool ConstVolt = false;
-    const double C = 0;
-    const double U0 = 0;
+//Properties of individual rails
+const t_simfloat l_r = 0;
+const t_simfloat w_r = 0;
+const t_simfloat h_r = 0;
+const t_simfloat dens_r = 0;
+const t_simfloat resistiv_r = 0;
+const t_simfloat SpecHeat_r = 0;
+
+//Properties of armature
+const t_simfloat l_a = 0;
+const t_simfloat w_a = 0;
+const t_simfloat h_a = 0;
+const t_simfloat dens_a = 0;
+const t_simfloat resistiv_a = 0;
+const t_simfloat SpecHeat_a = 0;
+const t_simfloat alpha_V = 0;
+const t_simfloat c_w_in = 0;
+const t_simfloat c_w_out = 0;
+
+//Properties of plates
+const t_simfloat h_pl = 0;
+const t_simfloat SpecHeat_pl = 0;
+
+//Properties of power wires
+const t_simfloat l_pw = 0;
+const t_simfloat A_pw = 0;
+const t_simfloat resistiv_pw = 0;
+
+//Properties of powersupply
+bool ConstPower = false;
+const t_simfloat C = 0;
+const t_simfloat U0 = 0;
+
+
+
+int main() {
 
     /*
         Calculations based on properties
